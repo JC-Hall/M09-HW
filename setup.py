b7c1cb5b20f64a8fdbe2d305baffb2ee74c74348
Joshua Hall <jch7aa@virginia.edum>
2024-11-01 21:05:25 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='M09 HW',
+    version='0.1',
+    description='DS5100 M09 Homework',
+    url='https://github.com/JC-Hall/M09-HW',
+    author='Josh',
+    author_email='jch7aa@virginia.edu',
+    license='MIT',
+    packages=find_packages()
+)
+
+
